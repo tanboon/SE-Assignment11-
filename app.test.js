9eb815b94612d1c172fa6d1f10cc4721e2750d7c
Tanboon Tiengtut <tanboon.top3837@gmail.com>
2024-04-23 18:40:36 +0700

    update app.js and app.test.js to have addMonthOfYear

--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ const { monthOfTheYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    expect( day ).toBe('Wednesday' );
 });
 test('getMonth returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
